a872508d6ff45dde41b1887a44af2c0779becf69
Ishikagupta02 <92158302+Ishikagupta02@users.noreply.github.com>
2022-05-29 22:48:35 +0530

    Add files via upload

---
+++ get a dict from an object field.py
@@ -0,0 +1,10 @@
+class dobj(object):
+     def _init_(self):
+          self.x='black'
+          self.y='yellow'
+          self.z='white'
+          def do_nothing(self):
+               pass
+          test=dobj()
+          print(text._dict_)
+          
