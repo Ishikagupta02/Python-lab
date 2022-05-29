da9222afccb2b452ddbbb46f5710900db7629ce1
Ishikagupta02 <92158302+Ishikagupta02@users.noreply.github.com>
2022-05-29 22:48:11 +0530

    Add files via upload

---
+++ count the value associated with key in a dict.py
@@ -0,0 +1,5 @@
+student=[{'id':1,'sucess': true,'name':lary,
+         'id':2,'sucess': false,'name':rabi,
+         'id':3,'sucess': true,'name':alex}]
+um(d['id'] for d in student)
+um(d['sucess'] for d in student)
